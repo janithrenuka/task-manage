635d95f4259780a7a6dc558ce50d4177c11325d9
Janith Renuka <56946043+janithrenuka@users.noreply.github.com>
2021-10-03 20:04:56 +0530

    add files

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Doctor Appoinment',
+        day: 'May 5th at 2.30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Club Meet',
+        day: 'May 10 at 3.30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'French Class',
+        day: 'May 12th at 6.00pm',
+        reminder: true,
+    },
+    {
+        id: 4,
+        text: 'Spanish Class',
+        day: 'May 14 at 10.00am',
+        reminder: true,
+    },
+];
